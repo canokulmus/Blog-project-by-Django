bb4d80b09be073e29e34b8f8f7836eb814450ed7
canokulmus <can.okulmus.can@gmail.com>
2021-08-02 16:44:01 +0300

    first commit

---
+++ notes.py
@@ -0,0 +1,14 @@
+
+#  manage.py runserver    - server çalıştırma
+
+# manage.py createsuperuser - superuser oluşturma
+
+#  manage.py startapp <appName>  - app oluşturma
+
+# python manage.py makemigrations - yeni model eklendiğinde çalıştırmamız lazım 
+ 
+# python manage.py migrate - migrationstakileri uygular
+
+# python manage.py shell - django modüllerinin ekli olduğu python terminalini açar
+
+# python manage.py collectstatic
